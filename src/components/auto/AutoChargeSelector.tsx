970e12e0f404babeb96b9810dcdbcfe781f96240
JustSomeone1223 <73112679+JustSomeone1223@users.noreply.github.com>
2024-01-18 17:33:34 -0500

    This file probably won't be used in the actual game.

--- src/components/auto/AutoChargeSelector.tsx
+++ src/components/auto/AutoChargeSelector.tsx
@@ -1,44 +1,47 @@
-/* eslint-disable @next/next/no-img-element */
-import { Col, ListGroup, Row } from "react-bootstrap";
-import ScoreButton from "../mini/ScoreButton";
-import FailButton from "../mini/FailButton";
+//NOTE: THIS WILL LIKELY NOT BE USED IN THE FINAL PRODUCT AS THERE IS NOTHING STAGE RELATED IN AUTON
 
-interface Props {
-  selected: "balanced" | "docked" | "failed" | null;
-  handleSelection: (selection: "balanced" | "docked" | "failed" | null) => void;
-}
+// /* eslint-disable @next/next/no-img-element */
+// import { Col, ListGroup, Row } from "react-bootstrap";
+// import ScoreButton from "../mini/ScoreButton";
+// import FailButton from "../mini/FailButton";
 
-export default function AutoChargeSelector({
-  selected,
-  handleSelection,
-}: Props) {
-  return (
-    <div className="d-flex flex-column align-items-center">
-      <h1 className="text-center mb-3">Charge Station</h1>
+// interface Props {
+//   selected: "balanced" | "docked" | "failed" | null;
+//   handleSelection: (selection: "balanced" | "docked" | "failed" | null) => void;
+// }
 
-      <ListGroup className="charge-selector text-center w-75 fs-5">
-        <ListGroup.Item
-          action
-          active={selected == "balanced"}
-          onMouseDown={() => handleSelection("balanced")}
-        >
-          Balanced
-        </ListGroup.Item>
-        <ListGroup.Item
-          action
-          active={selected == "docked"}
-          onMouseDown={() => handleSelection("docked")}
-        >
-          Docked
-        </ListGroup.Item>
-        <ListGroup.Item
-          action
-          active={selected == "failed"}
-          onMouseDown={() => handleSelection("failed")}
-        >
-          Failed
-        </ListGroup.Item>
-      </ListGroup>
-    </div>
-  );
-}
+
+// export default function AutoChargeSelector({
+//   selected,
+//   handleSelection,
+// }: Props) {
+//   return (
+//     <div className="d-flex flex-column align-items-center">
+//       <h1 className="text-center mb-3">Charge Station</h1>
+
+//       <ListGroup className="charge-selector text-center w-75 fs-5">
+//         <ListGroup.Item
+//           action
+//           active={selected == "balanced"}
+//           onMouseDown={() => handleSelection("balanced")}
+//         >
+//           Balanced
+//         </ListGroup.Item>
+//         <ListGroup.Item
+//           action
+//           active={selected == "docked"}
+//           onMouseDown={() => handleSelection("docked")}
+//         >
+//           Docked
+//         </ListGroup.Item>
+//         <ListGroup.Item
+//           action
+//           active={selected == "failed"}
+//           onMouseDown={() => handleSelection("failed")}
+//         >
+//           Failed
+//         </ListGroup.Item>
+//       </ListGroup>
+//     </div>
+//   );
+// }
