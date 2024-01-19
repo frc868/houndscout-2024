003a0564799f5dbc6f9ae5547a7b70b9a90cc203
JustSomeone1223 <73112679+JustSomeone1223@users.noreply.github.com>
2024-01-19 16:33:13 -0500

    Commented this entire file as it isn't going to be used.

--- src/components/prematch/PreloadSelector.tsx
+++ src/components/prematch/PreloadSelector.tsx
@@ -1,29 +1,31 @@
-/* eslint-disable @next/next/no-img-element */
+//THIS WILL NOT BE USED AS THERE IS ONLY ONE POSSIBLE PRELOAD.
 
-interface Props {
-  selected: "CONE" | "CUBE";
-  handleSelection: (selected: "CONE" | "CUBE") => void;
-  className?: string;
-}
+// /* eslint-disable @next/next/no-img-element */
 
-export default function PreloadSelector({
-  selected,
-  handleSelection,
-  className,
-}: Props) {
-  return (
-    <div className={`d-flex flex-column align-items-center ${className || ""}`}>
-      <h1 className="text-center mb-3">Preload</h1>
+// interface Props {
+//   selected: "CONE" | "CUBE";
+//   handleSelection: (selected: "CONE" | "CUBE") => void;
+//   className?: string;
+// }
 
-      <img
-        className="mx-auto my-2"
-        alt=""
-        src={`/assets/${selected.toLowerCase()}.png`}
-        width={65}
-        onMouseDown={() => {
-          handleSelection(selected === "CONE" ? "CUBE" : "CONE");
-        }}
-      />
-    </div>
-  );
-}
+// export default function PreloadSelector({
+//   selected,
+//   handleSelection,
+//   className,
+// }: Props) {
+//   return (
+//     <div className={`d-flex flex-column align-items-center ${className || ""}`}>
+//       <h1 className="text-center mb-3">Preload</h1>
+
+//       <img
+//         className="mx-auto my-2"
+//         alt=""
+//         src={`/assets/${selected.toLowerCase()}.png`}
+//         width={65}
+//         onMouseDown={() => {
+//           handleSelection(selected === "CONE" ? "CUBE" : "CONE");
+//         }}
+//       />
+//     </div>
+//   );
+// }
