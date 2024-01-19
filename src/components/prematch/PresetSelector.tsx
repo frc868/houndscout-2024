8a3c7d73e6b7682fc99e89b1f5938b592cb37e5c
JustSomeone1223 <73112679+JustSomeone1223@users.noreply.github.com>
2024-01-19 16:33:45 -0500

    Commented.

--- src/components/prematch/PresetSelector.tsx
+++ src/components/prematch/PresetSelector.tsx
@@ -1,31 +1,33 @@
-/* eslint-disable @next/next/no-img-element */
+//WONT BE USED
 
-interface Props {
-  selected: ("CONE" | "CUBE")[];
-  handleSelection: (selected: ("CONE" | "CUBE")[]) => void;
-}
+// /* eslint-disable @next/next/no-img-element */
 
-export default function PresetSelector({ selected, handleSelection }: Props) {
-  return (
-    <div className="d-flex flex-column align-items-center">
-      <h1 className="text-center mb-3">Preset Pieces</h1>
+// interface Props {
+//   selected: ("CONE" | "CUBE")[];
+//   handleSelection: (selected: ("CONE" | "CUBE")[]) => void;
+// }
 
-      {selected.map((item, idx) => (
-        <img
-          key={idx}
-          className="mx-auto my-2"
-          alt=""
-          src={`/assets/${item.toLowerCase()}.png`}
-          width={65}
-          onMouseDown={() => {
-            handleSelection(
-              selected.map((value, index) =>
-                index === idx ? (value === "CONE" ? "CUBE" : "CONE") : value
-              )
-            );
-          }}
-        />
-      ))}
-    </div>
-  );
-}
+// export default function PresetSelector({ selected, handleSelection }: Props) {
+//   return (
+//     <div className="d-flex flex-column align-items-center">
+//       <h1 className="text-center mb-3">Preset Pieces</h1>
+
+//       {selected.map((item, idx) => (
+//         <img
+//           key={idx}
+//           className="mx-auto my-2"
+//           alt=""
+//           src={`/assets/${item.toLowerCase()}.png`}
+//           width={65}
+//           onMouseDown={() => {
+//             handleSelection(
+//               selected.map((value, index) =>
+//                 index === idx ? (value === "CONE" ? "CUBE" : "CONE") : value
+//               )
+//             );
+//           }}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
