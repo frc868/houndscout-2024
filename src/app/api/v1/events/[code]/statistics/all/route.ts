44194c855fdbc3c87a2aac08262a90099baf1cc6
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-03-27 01:00:57 -0400

    Add simple full data export

---
+++ src/app/api/v1/events/[code]/statistics/all/route.ts
@@ -0,0 +1,197 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import {
+  ClimbType,
+  Event,
+  IncapSegment,
+  ScoringLocation,
+  StageAttempt,
+  TeamScore,
+  TeleopScoringEvent,
+} from "@prisma/client";
+import { Ranking } from "@/lib/enums";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { code: string } }
+) {
+  let event;
+  try {
+    event = await prisma.event.findUniqueOrThrow({
+      where: {
+        code: params.code,
+      },
+      include: {
+        teams: true,
+        matches: {
+          include: {
+            red1Team: true,
+            red2Team: true,
+            red3Team: true,
+            blue1Team: true,
+            blue2Team: true,
+            blue3Team: true,
+            red1TeamScore: {
+              include: {
+                team: true,
+                teleopScoringEvents: true,
+                incapSegments: true,
+                scouter: true,
+              },
+            },
+            red2TeamScore: {
+              include: {
+                team: true,
+                teleopScoringEvents: true,
+                incapSegments: true,
+                scouter: true,
+              },
+            },
+            red3TeamScore: {
+              include: {
+                team: true,
+                teleopScoringEvents: true,
+                incapSegments: true,
+                scouter: true,
+              },
+            },
+            blue1TeamScore: {
+              include: {
+                team: true,
+                teleopScoringEvents: true,
+                incapSegments: true,
+                scouter: true,
+              },
+            },
+            blue2TeamScore: {
+              include: {
+                team: true,
+                teleopScoringEvents: true,
+                incapSegments: true,
+                scouter: true,
+              },
+            },
+            blue3TeamScore: {
+              include: {
+                team: true,
+                teleopScoringEvents: true,
+                incapSegments: true,
+                scouter: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    let matches = event.matches;
+
+    const teamScoresWithDetails = matches
+      .flatMap((match) => [
+        ...(match.red1TeamScore
+          ? [
+              {
+                ...match.red1TeamScore,
+                teamNumber: match.red1Team.number,
+                station: "red1",
+                matchName: match.name,
+              },
+            ]
+          : []),
+        ...(match.red2TeamScore
+          ? [
+              {
+                ...match.red2TeamScore,
+                teamNumber: match.red2Team.number,
+                station: "red2",
+                matchName: match.name,
+              },
+            ]
+          : []),
+        ...(match.red3TeamScore
+          ? [
+              {
+                ...match.red3TeamScore,
+                teamNumber: match.red3Team.number,
+                station: "red3",
+                matchName: match.name,
+              },
+            ]
+          : []),
+        ...(match.blue1TeamScore
+          ? [
+              {
+                ...match.blue1TeamScore,
+                teamNumber: match.blue1Team.number,
+                station: "blue1",
+                matchName: match.name,
+              },
+            ]
+          : []),
+        ...(match.blue2TeamScore
+          ? [
+              {
+                ...match.blue2TeamScore,
+                teamNumber: match.blue2Team.number,
+                station: "blue2",
+                matchName: match.name,
+              },
+            ]
+          : []),
+        ...(match.blue3TeamScore
+          ? [
+              {
+                ...match.blue3TeamScore,
+                teamNumber: match.blue3Team.number,
+                station: "blue3",
+                matchName: match.name,
+              },
+            ]
+          : []),
+      ])
+      .filter((teamScore) => teamScore.submitted)
+      .map((teamScore) => ({
+        ...teamScore,
+        teamNumber: teamScore.teamNumber,
+        speakerScored: teamScore.teleopScoringEvents.filter(
+          (event) =>
+            event.scoringLocation === ScoringLocation.SPEAKER &&
+            !event.failedScoring
+        ).length,
+        speakerMissed: teamScore.teleopScoringEvents.filter(
+          (event) =>
+            event.scoringLocation === ScoringLocation.SPEAKER &&
+            event.failedScoring
+        ).length,
+        ampScored: teamScore.teleopScoringEvents.filter(
+          (event) =>
+            event.scoringLocation === ScoringLocation.AMP &&
+            !event.failedScoring
+        ).length,
+        ampMissed: teamScore.teleopScoringEvents.filter(
+          (event) =>
+            event.scoringLocation === ScoringLocation.AMP && event.failedScoring
+        ).length,
+        dropped: teamScore.teleopScoringEvents.filter((event) => event.dropped)
+          .length,
+      }))
+      .map((teamScore) => {
+        const {
+          team,
+          id,
+          teamId,
+          submitted,
+          scouterId,
+          scouter,
+          incapSegments,
+          teleopScoringEvents,
+          ...rest
+        } = teamScore;
+        return rest;
+      });
+
+    return NextResponse.json(teamScoresWithDetails);
+  } catch (e) {
+    return NextResponse.json({ ok: false });
+  }
+}
