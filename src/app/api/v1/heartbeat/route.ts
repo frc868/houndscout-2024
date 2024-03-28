7d463bf67a8985d396f2b08c96789446abb046d3
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-03-27 20:04:07 -0400

    Move next response into try catch for heartbeats

--- src/app/api/v1/heartbeat/route.ts
+++ src/app/api/v1/heartbeat/route.ts
@@ -3,21 +3,20 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(req: Request) {
-  let heartbeats;
   try {
-    heartbeats = await prisma.heartbeat.findMany();
+    const heartbeats = await prisma.heartbeat.findMany();
+    return NextResponse.json({
+      heartbeats: {
+        red1: heartbeats.find((h) => h.station === Station.RED1),
+        red2: heartbeats.find((h) => h.station === Station.RED2),
+        red3: heartbeats.find((h) => h.station === Station.RED3),
+        blue1: heartbeats.find((h) => h.station === Station.BLUE1),
+        blue2: heartbeats.find((h) => h.station === Station.BLUE2),
+        blue3: heartbeats.find((h) => h.station === Station.BLUE3),
+      },
+      ok: true,
+    });
   } catch {
     return NextResponse.json({ ok: false }, { status: 400 });
   }
-  return NextResponse.json({
-    heartbeats: {
-      red1: heartbeats.find((h) => h.station === Station.RED1),
-      red2: heartbeats.find((h) => h.station === Station.RED2),
-      red3: heartbeats.find((h) => h.station === Station.RED3),
-      blue1: heartbeats.find((h) => h.station === Station.BLUE1),
-      blue2: heartbeats.find((h) => h.station === Station.BLUE2),
-      blue3: heartbeats.find((h) => h.station === Station.BLUE3),
-    },
-    ok: true,
-  });
 }
