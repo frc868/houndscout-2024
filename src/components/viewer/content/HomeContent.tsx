55788974237d6b7a584106e7ceb97242dee7cbc7
JustSomeone1223 <73112679+JustSomeone1223@users.noreply.github.com>
2025-01-10 15:57:07 -0600

    minor edit

--- src/components/viewer/content/HomeContent.tsx
+++ src/components/viewer/content/HomeContent.tsx
@@ -4,8 +4,9 @@ export default function HomeContent() {
       style={{
         height: "calc(100% - 2*24px)",
         width: "calc(100% - 2*24px)",
+        color: "white",
       }}
-      className="m-4 bg-dark rounded-3"
+      className="m-4 bg-dark rounded-3 font-monospace text-center"
     >
       <h1>Welcome to the HoundScout data viewer!</h1>
     </div>
