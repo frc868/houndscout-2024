385597d2c20016a141d12dcce1708ec4eda4a18b
JustSomeone1223 <73112679+JustSomeone1223@users.noreply.github.com>
2025-01-30 11:30:14 -0500

    Sandbox MiniToggleBox fixes

--- src/components/sandbox/mini/MiniToggleBox.tsx
+++ src/components/sandbox/mini/MiniToggleBox.tsx
@@ -1,7 +1,7 @@
 interface Props {
   name: string;
   enabled: boolean;
-  handleClick: () => void;
+  //handleClick: () => void;
   className?: string;
 }
 
@@ -9,7 +9,7 @@ interface Props {
 export default function ToggleBox({
   name,
   enabled,
-  handleClick,
+  //handleClick,
   className,
 }: Props) {
   return (
@@ -25,7 +25,7 @@ export default function ToggleBox({
           height: "60px",
           fontSize: "30pt",
         }}
-        onMouseDown={handleClick}
+        //onMouseDown={handleClick}
       >
         {enabled && <i className="bi bi-check-lg" />}
       </div>
